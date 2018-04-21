6f8a98ebafa3fbec802ab56b3d82cd5827cec1f5
German Riveros Sepúlveda <germanrs@MacBook-Pro-de-German.local>
2018-04-21 03:55:56 -0300

    segunda línea

--- ejemploNetbeans/src/principal/Principal.java
+++ ejemploNetbeans/src/principal/Principal.java
@@ -7,6 +7,8 @@ public class Principal {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo!!");
+
+        System.out.println("Esta la segunda línea!");
         
     }
     
